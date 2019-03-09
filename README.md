50a88872f45951b12de977fa8e87360f9f87f540
Phillip Shebel <phil.shebel@gmail.com>
2019-03-09 10:58:13 -0500

    added core functionality

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
-### Javascript Object Exercise
+TODO
 
-This assignment is designed to provide practice with Javascript data structures, particularly
-objects. There are two parts to this assignment. First, you will create an object containing
-a list of 4 books. Each object will contain a title, author, and readingStatus. You will
-then create a button that organizes the object according to readingStatus. Second, you will
-create three inputs and a button. These inputs will take the same information from new books you want to enter,
-and then add them to the list of already organized book information when the button is clicked.
-
-1. You will have to remember how to create event listeners and attach functions.
-2. Remember Object format!!!
-3. Don't forget about your HTML and Javascript selectors!!!
-4. Refer to previous lessons/assignments for instructions on cloning code and getting started.
-
-### Extra Tasks
-
-1. You will be making an array helper, which will take all of the unread books and display in array format
-2. You will do the same but for read books
-3. You will create an update array button, that will update
-   both arrays with any new books added in the object generator since arrays were first created
+1) Get the smallest player
+2) Order the players by number using the sort function
+3) Find a player by number
+4) Edit a player
+   Using the find a player method, change player attributes based on user input.
--- script.js
+++ script.js
@@ -1,7 +1,83 @@
 let roster = [
-  {id: 0, name: "Ovechkin", number: 8, age: 33, weight: 239},
-  {id: 1, name: "Backstrom", number: 19, age: 30, weight: 200},
-  {id: 2, name: "Oshie", number: 77, age: 29, weight: 205},
-  {id: 3, name: "Kuznetsov", number: 92, age: 25, weight: 204},
-  {id: 4, name: "Carlson", number: 74, age: 29, weight: 215}
+  {name: "Ovechkin", number: 8, age: 33, weight: 239},
+  {name: "Backstrom", number: 19, age: 30, weight: 200},
+  {name: "Oshie", number: 77, age: 29, weight: 205},
+  {name: "Kuznetsov", number: 92, age: 25, weight: 204},
+  {name: "Carlson", number: 74, age: 29, weight: 215}
 ];
+
+addPlayer = (name, number, age, weight) => {
+  let newPlayer = {
+    name: '',
+    number: '',
+    age: '',
+    weight: ''
+  }
+  
+  newPlayer.name = name;
+  if (!isNaN(number)) {
+    newPlayer.number = number;
+  } else {
+    alert("Invalid number. Must be an integer")
+    return
+  }
+  if (!isNaN(age)) {
+    newPlayer.age = age;
+  } else {
+    alert("Invalid age. Must be an integer")
+    return
+  }
+
+  if (!isNaN(weight)) {
+    newPlayer.weight = weight;
+  } else {
+    alert("Invalid weight. Must be an integer")
+    return
+  }
+
+  roster.push(newPlayer)
+}
+
+handleAddPlayer = () => {
+  // getting values from html
+  let docName = document.getElementById('name').value;
+  let docNumber = document.getElementById('number').value;
+  let docAge = document.getElementById('age').value;
+  let docWeight = document.getElementById('weight').value;
+
+  // change roster value
+  addPlayer(docName, docNumber, docAge, docWeight)
+
+  // update html values
+  resetForm();
+  updateDocument();
+}
+
+getOldestPlayer = () => {
+  let oldestPlayer = roster[0]
+  for (let i = 0; i < roster.length; i++) {
+    let currentPlayer = roster[i]
+    if (currentPlayer.age > oldestPlayer.age) {
+      oldestPlayer = currentPlayer
+    }
+  }
+  return oldestPlayer
+}
+
+handleGetOldestPlayer = () => {
+  let oldestPlayer = getOldestPlayer()
+  let docOldestPlayer = document.getElementById('oldestPlayer')
+  docOldestPlayer.innerHTML = JSON.stringify(oldestPlayer)
+}
+
+resetForm = () => {
+  document.getElementById('name').value = '';
+  document.getElementById('number').value = '';
+  document.getElementById('age').value = '';
+  document.getElementById('weight').value = '';
+}
+
+updateDocument = () => {
+  let docRoster = document.getElementById('roster');
+  docRoster.innerHTML = JSON.stringify(roster);
+}
